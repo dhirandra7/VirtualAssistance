bc33bd67062d7760429515802b01d797b6d9c829
dhirendra <dhirandrapandey7@gmail.com>
2025-11-27 22:33:38 +0530

    Updated backend and frontend for auth routes and login/signup fixes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,15 @@
-import express from "express"
-import { Login, logOut, signUp } from "../controllers/auth.controllers.js"
+import express from "express";
+import { Login, logOut, signUp } from "../controllers/auth.controllers.js";
 
-const authRouter=express.Router()
+const authRouter = express.Router();
 
-authRouter.post("/signup",signUp)
-authRouter.post("/signin",Login)
-authRouter.get("/logout",logOut)
-export default authRouter
+// Signup
+authRouter.post("/signup", signUp);
+
+// Login (changed from /signin to /login to match frontend)
+authRouter.post("/login", Login);
+
+// Logout
+authRouter.get("/logout", logOut);
+
+export default authRouter;
